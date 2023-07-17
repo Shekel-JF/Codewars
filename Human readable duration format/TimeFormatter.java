cfcdb10fa9f5b175113281998e405f0c8f813093
Shekel-JF <129303182+Shekel-JF@users.noreply.github.com>
2023-07-17 13:02:11 +0200

    Add files via upload

--- Human readable duration format/TimeFormatter.java
+++ Human readable duration format/TimeFormatter.java
@@ -1,64 +1,57 @@
-import java.util.*;
-public class TimeFormatter {
-    
-    public static String formatDuration(int seconds) {
-        // your code goes here
-      
-        if(seconds==0)
+public class TimeFormatter
+{  
+    public static String formatDuration(int seconds)
+    {
+        if(seconds == 0)
         {
-            return "now";  
+            return "now";
         }
-        
-        String Y = Integer.toString(seconds/ 31536000);
-        String D = Integer.toString((seconds % 31536000) / 86400);
-        String H = Integer.toString(((seconds % 31536000) % 86400) / 3600);
-        String M = Integer.toString((((seconds % 31536000) % 86400) % 3600) / 60);
-        String S = Integer.toString((((seconds % 31536000) % 86400) % 3600) % 60);
-        
-        String[] strs = {Y, D, H, M, S};
-        String[] labels = {" year", " day", " hour", " minute", " second"};
-        String res = "";
-        
-        
+        int years = seconds / 60 / 60 / 24 / 365;
+        int days = seconds / 60 / 60 / 24 % 365;
+        int hours = seconds / 60 / 60 % 24;
+        int minutes = seconds / 60 % 60;
+        int secs = seconds % 60;
+        String result = "";
+        int counter = 0;
+      
+        int[] times = new int[]{years, days, hours, minutes, secs};
+        String[] formats = new String[]{"year", "day", "hour", "minute", "second"};
+      
         for(int i = 0; i < 5; i++)
         {
-            if(!strs[i].equals("0"))
+            if(times[i] > 1)
             {
-                
-                if(!strs[i].equals("1"))
+                formats[i]+="s";
+            }
+            if(times[i] > 0)
+            {
+                counter++;
+            }
+        }
+             
+        for(int j = 0; j< 5; j++)
+        {
+            if(times[j] !=0)
+            {
+                result+= times[j] + " " + formats[j];
+            }
+            else
+            {
+                continue;
+            }
+            if(counter > 1)
+            {
+                if(counter == 2)
                 {
-                    labels[i]+= "s";  
-                } 
-                strs[i] = String.join("", strs[i], labels[i]);
+                    result+=" and ";
+                }
+                else
+                {
+                    result+=", ";
+                }
+                counter--;
             }
         }
-        List<String> wordList = new ArrayList<String>(Arrays.asList(strs));
-        Iterator<String> iter = wordList.iterator();
-        while(iter.hasNext())
-        {
-            String x = iter.next();
-            if(x.equals("0"))
-            {
-                iter.remove();
-            }
-        }
-        
-        for(int j = 0; j < wordList.size(); j++)
-        {
-            res+=wordList.get(j);
-            if(wordList.size() > 1 && j == wordList.size() -2)
-            {
-                res+=" and ";
-            }
-            else if(wordList.size() > 1 && j != wordList.size() -1)
-            {
-                res+=", ";
-            }
-        }
-        
-        return res;
-        
-        
-        
+    return result;
     }
 }
