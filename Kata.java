f59868a86b3e93be9dde8dc9bb74a225eab8d562
Shekel-JF <129303182+Shekel-JF@users.noreply.github.com>
2023-07-20 13:01:45 +0200

    Add files via upload

---
+++ Kata.java
@@ -0,0 +1,54 @@
+import java.util.*;
+public class Kata
+{
+    public static long nextBiggerNumber(long n)
+    {
+      String numString = Long.toString(n);
+      int length = numString.length();
+      char[] digits = numString.toCharArray();
+
+      for(int i = length - 1; i > 0; i--)
+      {
+          if(digits[i - 1] < digits[i])
+          {
+              int highestIndex = i;
+              for(int j = i; j < length; j++)
+              {
+
+                  if(digits[j] < digits[highestIndex] && digits[j] > digits[i-1])
+                  {
+                      highestIndex = j;
+                  }
+              }
+              char tmp = digits[i - 1];
+              digits[i - 1] = digits[highestIndex];
+              digits[highestIndex] = tmp;
+
+              char[] toSort = new char[length - i];
+              for(int k = 0; k < length - i; k++)
+              {
+                  toSort[k] = digits[length - 1 - k];
+              }
+              Arrays.sort(toSort);
+              String result = "";
+              for(int l = 0; l < length - toSort.length; l++)
+              {
+                  if(digits[0] == '0')
+                  {
+                      return -1;
+                  }
+                  result+=digits[l];
+              }
+              for(int m = 0; m < toSort.length; m++)
+              {
+                  result+=toSort[m];
+              }
+              if(Long.parseLong(result) > n)
+              {
+                  return Long.parseLong(result);
+              }
+          }   
+      }
+      return -1;
+    }
+}
