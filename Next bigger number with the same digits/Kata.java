4a047b605e7c79b77b5d8a32c7aa9f0ea646cc48
Shekel-JF <129303182+Shekel-JF@users.noreply.github.com>
2023-07-20 14:06:16 +0200

    Add files via upload

--- Next bigger number with the same digits/Kata.java
+++ Next bigger number with the same digits/Kata.java
@@ -1,52 +1,28 @@
-import java.util.*;
+import java.util.Arrays;
 public class Kata
 {
-    public static long nextBiggerNumber(long n)
+    public static long nextBiggerNumber(long n)  
     {
-      String numString = Long.toString(n);
-      int length = numString.length();
-      char[] digits = numString.toCharArray();
-
-      for(int i = length - 1; i > 0; i--)
+      System.out.println(n);
+      char [] s = String.valueOf(n).toCharArray();
+      for(int i = s.length - 2; i >= 0; i--)
       {
-          if(digits[i - 1] < digits[i])
-          {
-              int lowestIndex = i;
-              for(int j = i; j < length; j++)
-              {
-                  if(digits[j] < digits[lowestIndex] && digits[j] > digits[i-1])
-                  {
-                      lowestIndex = j;
-                  }
-              }
-              char tmp = digits[i - 1];
-              digits[i - 1] = digits[lowestIndex];
-              digits[lowestIndex] = tmp;
-
-              char[] toAdd = new char[length - i];
-              for(int k = 0; k < length - i; k++)
-              {
-                  toAdd[k] = digits[length - 1 - k];
-              }
-              Arrays.sort(toAdd);
-              String result = "";
-              for(int l = 0; l < length - toAdd.length; l++)
-              {
-                  if(digits[0] == '0')
-                  {
-                      return -1;
-                  }
-                  result+=digits[l];
-              }
-              for(int m = 0; m < toAdd.length; m++)
-              {
-                  result+=toAdd[m];
-              }
-              if(Long.parseLong(result) > n)
-              {
-                  return Long.parseLong(result);
-              }
-          }   
+           if(s[i] < s[i + 1])
+           {
+                int smallestIndex = i + 1;
+                for(int j = i + 1; j < s.length; j++)
+                { 
+                    if(s[j] < s[smallestIndex] && s[j] > s[i])
+                    {
+                        smallestIndex = j;
+                    }
+                }
+                char tmp = s[i];
+                s[i] = s[smallestIndex];
+                s[smallestIndex] = tmp;
+                Arrays.sort(s, i + 1, s.length);
+                return Long.valueOf(String.valueOf(s));          
+           }
       }
       return -1;
     }
