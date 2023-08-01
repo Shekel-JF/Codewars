14645efdc759eb761b3640e9f762f0263a31e14b
Shekel-JF <129303182+Shekel-JF@users.noreply.github.com>
2023-08-01 21:42:34 +0200

    Add files via upload

--- Perimeter of squares in a rectangle/SumFct.java
+++ Perimeter of squares in a rectangle/SumFct.java
@@ -4,16 +4,16 @@ public class SumFct
 {
     public static BigInteger perimeter(BigInteger n)
     {
-        BigInteger three = BigInteger.valueOf(3);
-        BigInteger fibNPlus3 = fib(n.add(three));
-        BigInteger perimeter = fibNPlus3.subtract(BigInteger.ONE).multiply(BigInteger.valueOf(4));
+        BigInteger two = BigInteger.valueOf(2);
+        BigInteger fibNPlus2 = fib(n.add(two));
+        BigInteger perimeter = fibNPlus2.subtract(BigInteger.ONE).multiply(BigInteger.valueOf(4));
         return perimeter;
     }
     private static BigInteger fib(BigInteger n)
     {
         BigInteger a = BigInteger.ZERO;
         BigInteger b = BigInteger.ONE;
-        for (int i = 0; i < n.intValue(); i++)
+        for (int i = 0; i <= n.intValue(); i++)
         {
             BigInteger tmp = a.add(b);
             a = b;
