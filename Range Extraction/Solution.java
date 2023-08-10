64622e4b2001e2e7a5170273d6db8ad8645a08c1
Shekel-JF <129303182+Shekel-JF@users.noreply.github.com>
2023-08-10 13:01:42 +0200

    Add files via upload

---
+++ Range Extraction/Solution.java
@@ -0,0 +1,25 @@
+class Solution
+{
+	public static String rangeExtraction(int[] arr)
+    {
+        StringBuilder result = new StringBuilder();
+        result.append(arr[0]);
+        for(int i = 0; i < arr.length - 1; i++)
+        {
+            if(arr[i + 1] - arr[i] > 1 || i + 2 < arr.length && arr[i + 2] - arr[i + 1] > 1 || i + 2 >= arr.length)
+            {
+                result.append(",").append(arr[i + 1]);
+            }
+            else
+            {
+                while(i < arr.length - 1 && arr[i + 1] - arr[i] == 1)
+                {
+                    i++;
+                }
+                result.append("-").append(arr[i]);
+                i--;
+            }
+        }  
+    		return result.toString();
+    }
+}
